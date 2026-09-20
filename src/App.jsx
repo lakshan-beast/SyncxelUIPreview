ba987ee5e6e91d877784ccfaac97202b2b8aa7c2
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-09-20 13:20:57 +0530

    Update routing in App component to include landing page and footer preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FooterPreviewPage from "./pages/FooterPreviewPage";
+import LandingPage from "./pages/LandingPreview";
 import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FooterPreviewPage />} />
-        {/* <Route path="*" element={<PageNotFound />} /> */}
+        <Route path="/footer" element={<FooterPreviewPage />} />
+
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
